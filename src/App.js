97b2d1284a87e5f2fda467eea6717d3d9e9bbe4e
valdir-ti <valdir.ti@gmail.com>
2020-05-22 13:15:58 -0300

    Inserindo a opção de favoritar e tirar o favorito dos repositórios listados

--- src/App.js
+++ src/App.js
@@ -13,10 +13,23 @@ export default function App() {
     fetchData();
   }, []);
 
+  //Inserindo a informação de favorito
+  function handleFavorite(id){
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
       <ul>
         {repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favorito) </span>}
+            &nbsp;&nbsp;<button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
         ))}
       </ul>
   );
