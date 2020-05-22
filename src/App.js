5d15c2cbaab251afae9358766fd4d82d35f58f47
valdir-ti <valdir.ti@gmail.com>
2020-05-22 11:51:24 -0300

    Utilizando o useState como gerenciador de estado do componente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-  return <h1>Hello World!</h1>;
+  const [ repositories, setRepositories ] = useState([
+    { id: 1, name: 'Repos teste'},
+    { id: 2, name: 'Repos 2 '},
+    { id: 3, name: 'Repos 3'} 
+  ]);
+
+  function handleAddRepository(){
+    setRepositories([
+      ...repositories, 
+      { id: Math.random(), name: 'Novo Repo'} 
+    ]);
+  }
+
+  return (
+    <>      
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>
+        Adicionar repositório
+      </button>
+    </>
+  );
 }
